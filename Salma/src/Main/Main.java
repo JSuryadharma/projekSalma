9b05c01e62b652044cf116e98c893efbb117ab28
JSuryadharma <jsuryadharma9@gmail.com>
2020-04-02 00:05:44 +0700

    Main Menu, dan Data-data dasar
    Penambahan dasar untuk program Salma, beserta GUI basic. Oper ke Ray Christian, goodluck bro!

--- Salma/src/Main/Main.java
+++ Salma/src/Main/Main.java
@@ -1,10 +1,16 @@
 package Main;
 
+import java.util.ArrayList;
+
+import Menu.MainMenu;
+import User.User;
+
 public class Main {
+	
+	public static ArrayList<User> userdata = new ArrayList<User>();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new MainMenu();
 	}
 
 }
---
+++ Salma/src/User/User.java
@@ -0,0 +1,54 @@
+package User;
+
+public class User {
+	private String UUID;
+	private String nama;
+	private String email;
+	private String password;
+	
+	
+	public User(String UUID, String nama, String email, String password) {
+		super();
+		this.UUID = UUID;
+		this.nama = nama;
+		this.email = email;
+		this.password = password;
+	}
+
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getUUID() {
+		return UUID;
+	}
+
+	public void setUUID(String uUID) {
+		UUID = uUID;
+	}
+
+	public Boolean auth(String email, String password){
+		return this.email.equals(email)&& this.password.equals(password);
+	}
+	
+	public Boolean authEmail(String email){
+		return this.email.equals(email);
+	}
+	
+}
