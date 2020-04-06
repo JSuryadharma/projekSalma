15ae23e59c9cab38693a954a96ec268738a05acc
rc144 <63002637+rc144@users.noreply.github.com>
2020-04-06 10:35:49 +0700

    pushulang
    pushulang

--- Salma/src/User/Gold.java
+++ Salma/src/User/Gold.java
@@ -2,6 +2,11 @@ package User;
 
 public class Gold extends PremiumCustomer {
 
+	public Gold(String UUID, String nama, String email, String password) {
+		super(UUID, nama, email, password);
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public int addPoint() {
 		// TODO Auto-generated method stub
